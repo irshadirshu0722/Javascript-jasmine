dd2ab552999b402413fec9c9088a6ab85d16d367
prowebking <irshusvlog@gmail.com>
2024-07-26 18:55:00 +0530

    initial commit

---
+++ spec/moneyTest.js
@@ -0,0 +1,14 @@
+import { formatCurrency } from "../src/money.js";
+
+console.log("Hi");
+describe("test suite : formatCurrency", () => {
+  it("converts cents into dollar ", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+  it("work with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  it("round upto nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+});
